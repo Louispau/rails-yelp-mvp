06c17c107dc1e4c99ef62a6da4138a182077e98a
Louis Paulsmeier <louis-paulsmeier@hotmail.de>
2020-11-11 16:26:57 +0100

    is kind of working now...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Clearning the database'
+Restaurant.destroy_all
+
+categories = %w[chinese italian japanese french belgian]
+
+puts 'Creating restaurants'
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: categories.sample
+  )
+  puts restaurant
+end
+puts 'Finished'
+
+# Or
+
+# puts “Cleaning the database”
+# Restaurant.destroy_all
+# puts “Creating Restaurant”
+# Restaurant.create!(name: “Brenner”, address: “Altstadt”, phone_number: “08953421”, category: “french”)
+# Restaurant.create!(name: “Chuchin”, address: “Bogenhausen”, phone_number: “08989701”, category: “chinese”)
+# Restaurant.create!(name: “Kokumi”, address: “Lehel”, phone_number: “089334563”, category: “japanese”)
+# Restaurant.create!(name: “Osteria”, address: “Max-Vorstadt”, phone_number: “089567754”, category: “italian”)
+# Restaurant.create!(name: “OccamDeli”, address: “Schwabing”, phone_number: “0899204”, category: “french”)
+# puts “Finished!”
